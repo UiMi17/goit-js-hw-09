9707833d8ff0d27d9654ae0a3dd8af4b995172e7
Dan Dudaiev <roffrooff@gmail.com>
2023-03-23 15:56:27 +0200

    task-01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  const bodyColorInterval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(bodyColorInterval);
+  });
+});
