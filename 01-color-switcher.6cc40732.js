15658573db55987862d18db21bfa359f3fa746d1
UiMi17 <122373927+UiMi17@users.noreply.github.com>
2023-03-23 13:59:55 +0000

    Deploying to gh-pages from @ UiMi17/goit-js-hw-09@33a6ad3ef3bc7d6456ed4127d2e6bb9f62145eca 🚀

---
+++ 01-color-switcher.6cc40732.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1;var d=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3);e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}))}))}();
+//# sourceMappingURL=01-color-switcher.6cc40732.js.map
