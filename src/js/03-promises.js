d2315de989731baadb6b22d70b0a710ec2e879f8
Dan Dudaiev <roffrooff@gmail.com>
2023-03-24 21:01:25 +0200

    hw-final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const promiseGeneratorForm = document.querySelector('.form');
+
+promiseGeneratorForm.addEventListener('submit', ev => {
+  ev.preventDefault();
+
+  const generatorFormAmount = Number(promiseGeneratorForm.amount.value);
+  const generatorFormFirstStep = Number(promiseGeneratorForm.delay.value);
+  const generatorFormStep = Number(promiseGeneratorForm.step.value);
+
+  let currentStep = 0;
+
+  for (let i = 0; i < generatorFormAmount; i++) {
+    let delay = generatorFormFirstStep;
+
+    if (i !== 0) {
+      currentStep += generatorFormStep;
+      delay += currentStep;
+    }
+
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          {
+            timeout: 2500,
+            cssClass: 'success',
+          }
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          {
+            timeout: 2500,
+            cssClass: 'failure',
+          }
+        );
+      });
+  }
+  promiseGeneratorForm.reset();
+});
